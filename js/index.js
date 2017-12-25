98bd9f7734baf243d35b2583b8b917095e112cc2
VB42 <vbhaip01@gmail.com>
2017-12-24 22:27:17 -0500

    Added null paths to logo json

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//$(document).ready(function() {
+  //var $e = $("#logo");
+  //console.log($e[0].contentDocument);
+  //$e.replaceWith($($e[0].contentDocument.documentElement.body.section).clone());
+//
+//});
