6ef65a3d5a604c5c982d3c14b02427e207982025
VB42 <vbhaip01@gmail.com>
2017-12-24 22:14:13 -0500

    Changed to svg inline written out for now

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
-  var $e = $("#logo");
-  $e.replaceWith($($e[0].contentDocument.documentElement).clone());
-});
+//$(document).ready(function() {
+  //var $e = $("#logo");
+  //console.log($e[0].contentDocument);
+  //$e.replaceWith($($e[0].contentDocument.documentElement.body.section).clone());
+//
+//});
