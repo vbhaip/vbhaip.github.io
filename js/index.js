09e3bfe4784151dfcebc310ea358cdb75d374b3e
VB42 <vbhaip01@gmail.com>
2017-12-24 22:22:08 -0500

    Minor edits

--- js/index.js
+++
@@ -1,6 +0,0 @@
-//$(document).ready(function() {
-  //var $e = $("#logo");
-  //console.log($e[0].contentDocument);
-  //$e.replaceWith($($e[0].contentDocument.documentElement.body.section).clone());
-//
-//});
