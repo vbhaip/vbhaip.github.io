c0c1b95dd715a576203704fc082a40884ff8b1da
VB42 <vbhaip01@gmail.com>
2017-12-27 11:54:01 -0500

    Changed animation file

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   spirit.setup();
 
-  spirit.load('animations/animations.json').then(groups => {
+  spirit.load('animations/animationsv2.json').then(groups => {
     const gears = spirit.groups.get("gears");
     const arrows = spirit.groups.get("arrows");
     const logo = spirit.groups.get("logo");
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
 $("#menu").onClick(function(){
 
-  spirit.load('animations/animations.json').then(groups => {
+  spirit.load('animations/animationsv2.json').then(groups => {
     const menu = spirit.groups.get("menu");
 
     menu.construct().play();
