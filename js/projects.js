6ef36205bf0980f57134e478f282932401cc2f5a
VB42 <vbhaip01@gmail.com>
2017-12-28 16:23:20 -0500

    Added projects page in dev

---
+++ js/projects.js
@@ -0,0 +1,43 @@
+var menuClick = false;
+
+$(document).ready(function() {
+
+  spirit.setup();
+  spirit.load('animations/animationsv2.json').then(groups => {
+    const arrows = spirit.groups.get("arrows");
+
+    arrows.construct().repeat(-1).play();
+
+  });
+  hoverArrow($(".leftnav"), $(".back"));
+
+
+});
+
+function clickMenu() {
+  const menu = spirit.groups.get("menu");
+  if (!menuClick) {
+    menu.construct().play();
+    menuClick = true;
+
+    $(".menuselector").css("height", "100%");
+  } else {
+    menu.construct().reverse(0);
+    menuClick = false;
+
+    $(".menuselector").css("height", "0%");
+  }
+}
+
+
+function hoverArrow(nav, arrowtext) {
+
+  nav.hover(function(){
+    arrowtext.css("text-decoration", "underline");
+
+  }, function(){
+    arrowtext.css("text-decoration", "none");
+  });
+
+
+}
