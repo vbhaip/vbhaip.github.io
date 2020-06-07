f5b6b1e3c7befd18d60934afca59bc35874b747d
vbhaip <vbhaip01@gmail.com>
2020-06-07 18:36:29 -0400

    animated logo, fade ins look nicer

---
+++ new/js/main.js
@@ -0,0 +1,10 @@
+const image = document.getElementById('pfp');
+new simpleParallax(image, {
+	orientation: 'right',
+	scale: 2.00,
+	overflow: true,
+});
+$(document).ready(function(){
+    $('#introtext').fadeIn();
+});
+
