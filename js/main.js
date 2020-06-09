9997e069c3c1039f01069a3d9bfb75af18ad8844
vbhaip <vbhaip01@gmail.com>
2020-06-08 21:12:23 -0400

    Formatted better for mobile, scroll fade in view

--- js/main.js
+++ js/main.js
@@ -8,3 +8,36 @@ $(document).ready(function(){
     $('#introtext').fadeIn();
 });
 
+
+
+
+const options = {delay: 0,
+duration: 2000,
+delay: 0,
+reset: false};
+
+const scroll_delay = 200;
+
+var options_temp = {...options};
+
+for(let i=0; i<$('#timeline li').length; i++){
+	ScrollReveal().reveal($('#timeline li:' + 'nth-child(' + (i+1).toString() + ')'), options_temp);
+	options_temp['delay'] = options_temp['delay'] + scroll_delay;
+}
+ScrollReveal().reveal($('#experience-bottom'), options_temp);
+
+options_temp = {...options};
+
+ScrollReveal().reveal($('#projects-header'), options_temp);
+options_temp['delay'] = scroll_delay;
+
+for(let i=0; i<$('.project-shape').length; i++){
+	ScrollReveal().reveal($('.project-shape:' + 'eq(' + (i+0).toString() + ')'), options_temp);
+	options_temp['delay'] = options_temp['delay'] + scroll_delay;
+}
+
+options_temp = {...options};
+
+ScrollReveal().reveal($('#contact_header_message'), options_temp);
+options_temp['delay'] = scroll_delay;
+ScrollReveal().reveal($('#contactform'), options_temp);
